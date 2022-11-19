4fdaf79b9ead911394211eaa700d77e44c3490ce
FlareZ <40903650+FlareZ123@users.noreply.github.com>
2022-11-18 19:53:56 -0500

    tests

--- webext/modules/transfer.js
+++ webext/modules/transfer.js
@@ -13,4 +13,34 @@
     node.appendChild(script);
 };
 
+if (window.location.href.includes("moomoo.io")) {
+    let removals = 0;
+    let observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            mutation.addedNodes.forEach(function(node) {
+                if (node.nodeName == "SCRIPT") {
+                    if (node.src.includes("/bundle.js") || /(cookiepro.com)/.exec(node.src)) {
+                        removals++;
+                        //console.log('murdering');
+                        try {
+                            node.parentNode.removeChild(node);
+                            //console.log('method 1');
+                        } catch(e) {
+                            node.remove();
+                            //console.log('method 2');
+                        };
+                        //removals == 2 && (observer.disconnect());
+                    };
+                };
+            });
+        });
+    });
+    observer.observe(document, {
+        attributes: true,
+        characterData: true,
+        childList: true,
+        subtree: true
+    });
+};
+
 injectScript(chrome.runtime.getURL('modules/script.js'), 'body');
