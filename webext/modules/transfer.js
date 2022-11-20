4e928089538180a9b693c2e5d0d794c891c672e6
FlareZ <40903650+FlareZ123@users.noreply.github.com>
2022-11-20 14:51:34 -0500

    tests

--- webext/modules/transfer.js
+++ webext/modules/transfer.js
@@ -7,12 +7,11 @@ console.log("fireAt");
  * @param  {type} tag The tag as string, where the script will be append (default: 'body').
  * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
  */
-function injectScript(file_path, tag) {
-    let node = document.getElementsByTagName(tag)[0];
+function injectScript(file_path) {
     let script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
     script.setAttribute('src', file_path);
-    node.appendChild(script);
+    document.documentElement.appendChild(script);
 };
 
-injectScript(chrome.runtime.getURL('modules/script.js'), 'body');
+injectScript(chrome.runtime.getURL('modules/script.js'));
